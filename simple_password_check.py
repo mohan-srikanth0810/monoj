070ef69c4deb264fb20c8f437d945b5af63ce0df
mohan-srikanth0810 <mohanmeenu0810@gmail.com>
2025-07-26 10:22:34 +0530

    Add files via upload

---
+++ simple_password_check.py
@@ -0,0 +1,9 @@
+# Simple password check
+PASSWORD = "Secret123"
+
+entered = input("Enter password: ")
+
+if entered == PASSWORD:
+    print("Correct password!")
+else:
+    print("Incorrect password.")
